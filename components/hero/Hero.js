1138f14fd5e17a1a60a8573707115d5ecadf2618
Pumpkin-spice-cloud <alan_mo@live.com>
2020-05-29 01:23:46 -0400

    style: decrease hero image quality and eliminate small side scroll
     - when width of element set to 100vw, and there is a vertical scroll
    bar, 100vw takes into account the vertical scroll bar as part of the
    view port, causing the
    addition of a horizontal scroll bar

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,12 +1,21 @@
 import style from "./style.module.scss";
 export default function Hero({ randomPosterUrl }) {
+  //this allows us to access poster of different sizes
+  //since the randomPosterUrl passed down is an url that points to a full sized img
+  //randomPosterUrl = "https://image.tmdb.org/t/p/original/eMAHXzZXoNzximFSYm2hn0LiPB0.jpg"
+  //const posterSubString = "eMAHXzZXoNzximFSYm2hn0LiPB0.jpg"
+  const posterSubString = randomPosterUrl.substring(
+    randomPosterUrl.lastIndexOf("/") + 1
+  );
+  const w780Poster = "https://image.tmdb.org/t/p/w780/" + posterSubString;
+  const w1280Poster = "https://image.tmdb.org/t/p/w780/" + posterSubString;
   return (
     <header className={style.header}>
       <span>
         Movie <br />
         Newsletter
       </span>
-      <img src={randomPosterUrl} />
+      <img src={w780Poster} />
     </header>
   );
 }
