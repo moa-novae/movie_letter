fa68f4f417b6521c217866972c8e2d9c22be1162
Pumpkin-spice-cloud <alan_mo@live.com>
2020-05-29 16:21:27 -0400

    remove unused files

--- db/genres.js
+++
@@ -1,7 +0,0 @@
-const db = require("./initialize").instance;
-
-function queryTopMovie() {
-  return db.any(`SELECT * FROM genre`);
-}
-
-module.exports = queryTopMovie;
--- db/top_movie.js
+++
@@ -1,7 +0,0 @@
-const db = require("../db/initialize").instance;
-
-function queryTopMovie() {
-  return db.any(`SELECT * FROM top_movie`);
-}
-
-module.exports = queryTopMovie;
